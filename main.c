85475b6adffd33fe1b0f6c40136335aa781383b4
Robert Vasek <robert.vasek@cern.ch>
2021-10-16 16:08:05 +0200

    main: added more cmd args

--- main.c
+++ main.c
@@ -70,23 +70,44 @@ char **tokenize(const char *src, int token, int *token_count) {
   return tokens;
 }
 
+void print_usage(const char *progname) {
+  printf("rados-reference-tracker is a proof-of-concept implementation of a "
+         "reference tracker for ceph-csi plugin.\n\n");
+
+  printf("Usage: %s -i CLIENT ID -p POOL NAME -c CEPH CONFIG FILE "
+         "[-r RT NAME] -k REF KEYS -o RT OPERATION [-h]\n",
+         progname);
+
+  printf("  -i CLIENT ID\t\tcephx client ID.\n");
+  printf("  -p POOL NAME\t\tCeph pool name.\n");
+  printf("  -c CEPH CONFIG FILE\tCeph config file.\n");
+  printf("  -r RT NAME\t\tName of the RADOS object for this reference tracker. "
+         "Defaults to 'hello-reference-tracker' if none provided.\n");
+  printf("  -k REF KEYS\t\tComma-separated list of keys to be used in the RT "
+         "operation.\n");
+  printf("  -o RT OPERATION\tAccepted values are 'add' and 'rem'. Specifies "
+         "what to do with provided keys. 'add' adds them to tracked "
+         "references, 'rem' removes them.\n");
+  printf("  -h\t\t\tThis help message.\n");
+}
+
 int main(int argc, const char **argv) {
   int ret = 0;
 
-  char *client_id = NULL;
-  char *pool_name = NULL;
-  char *config_file = NULL;
-  char *keys_str = NULL;
-  char *op_str = NULL;
+  const char *client_id = NULL;
+  const char *pool_name = NULL;
+  const char *config_file = NULL;
+  const char *keys_str = NULL;
+  const char *op_str = NULL;
+  const char *rt_name = NULL;
   rt_op_t op;
 
   rados_t rados = NULL;
-  rados_ioctx_t ioctx = NULL;
 
   // Parse reference-tracker command line options.
   {
     int c;
-    while ((c = getopt(argc, (char *const *)argv, "i:p:c:k:o:")) != -1) {
+    while ((c = getopt(argc, (char *const *)argv, "i:p:c:k:o:r:h")) != -1) {
       switch (c) {
       case 'i':
         client_id = optarg;
@@ -97,21 +118,32 @@ int main(int argc, const char **argv) {
       case 'c':
         config_file = optarg;
         break;
+      case 'r':
+        rt_name = optarg;
+        break;
       case 'k':
         keys_str = optarg;
         break;
       case 'o':
         op_str = optarg;
         break;
+      case 'h':
+        print_usage(argv[0]);
+        exit(0);
       }
     }
   }
 
   validate_not_empty("-i CLIENT ID", client_id);
+  validate_not_empty("-c CEPH CONFIG FILE", client_id);
   validate_not_empty("-p POOL NAME", pool_name);
   validate_not_empty("-k COMMA SEPARATED LIST OF KEYS", keys_str);
   validate_not_empty("-o OPERATION", op_str);
   op = validate_and_parse_op(op_str);
+
+  if (!rt_name || strlen(rt_name) == 0) {
+    rt_name = "hello-reference-tracker";
+  }
 
   int keys_count;
   char **keys = tokenize(keys_str, ',', &keys_count);
